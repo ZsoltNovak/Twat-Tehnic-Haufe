c85c11d4a29adae1e94da7641c7381801c3ddfae
ZsoltNovak <zsoltnovak1995@gmail.com>
2022-04-02 18:50:14 +0300

    Initial state of the client app

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Twat tehnic Haufe WebApp</div>;
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
