c16278bc2c1ca49993de3e603fe72a32c6e94206
ZsoltNovak <63663553+ZsoltNovak@users.noreply.github.com>
2022-04-04 10:54:53 +0300

    Added initial Server Setup and two endpoints, plus an aggregation that does not work yet. (#6)

--- server/src/index.js
+++ server/src/index.js
@@ -1,28 +1,88 @@
-"use strict";
-
 const Hapi = require("@hapi/hapi");
 
+// Reading our data set from zipcodes.json
+const fs = require("fs");
+const zipcodesData = fs.readFileSync("zipcodes.json");
+const zipcodes = JSON.parse(zipcodesData);
+
 const init = async () => {
+  // Initiating the Server
   const server = Hapi.server({
     port: 3001,
     host: "localhost",
   });
 
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: (request, h) => {
-      return "Hello World!";
+  // Connecting to MongoDB
+  await server.register({
+    plugin: require("hapi-mongodb"),
+    options: {
+      url: "mongodb+srv://user:ZXp7QALSSpH3saHz123@cluster0.ffnxh.mongodb.net/haufenDB?retryWrites=true&w=majority",
+      settings: {
+        useUnifiedTopology: true,
+      },
+      decorate: true,
     },
   });
 
+  // Checking if the connection with the MongoDB is working
+  server.route({
+    method: "GET",
+    path: "/connection",
+    handler: (req, h) => {},
+  });
+
+  // Get all zipcodes
+  server.route({
+    method: "GET",
+    path: "/zipcodes",
+    handler: async (req, h) => {
+      const offset = Number(req.query.offset) || 0;
+      const movies = await req.mongo.db
+        .collection("zipcodes")
+        .find({})
+        .sort({ pop: -1 })
+        .skip(offset)
+        .limit(20)
+        .toArray();
+      return movies;
+    },
+  });
+
+  // Add multiple zipcodes to the database
+  server.route({
+    method: "POST",
+    path: "/zipcodes",
+    handler: async (req, h) => {
+      const payload = zipcodes;
+      const status = await req.mongo.db
+        .collection("zipcodes")
+        .insertMany(payload);
+      return status;
+    },
+  });
+
+  // Executing aggregation based on population
+  server.route({
+    method: "GET",
+    path: "/population",
+    handler: (req, h) => {
+      const population = req.mongo.db.collection("zipcodes").aggregate([
+        {
+          $group: {
+            _id: { state: "$state", city: "$city" },
+            pop: { $sum: "$pop" },
+          },
+        },
+        { $group: { _id: "$_id.state", avgCityPop: { $avg: "$pop" } } },
+      ]);
+      console.log(population);
+      return population;
+    },
+  });
+
+  // Starting the Server
   await server.start();
   console.log("Server running on %s", server.info.uri);
 };
-
-process.on("unhandledRejection", (err) => {
-  console.log(err);
-  process.exit(1);
-});
 
 init();
